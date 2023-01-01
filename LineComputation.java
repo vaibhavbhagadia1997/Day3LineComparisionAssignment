0c2d540cabc1b83931e2832f30f956f1a386b021
vaibhavbhagadia@1999 <vaibhavbhagadia68@gmail.com>
2023-01-01 22:50:52 +0530

    Greeting

---
+++ LineComputation.java
@@ -0,0 +1,11 @@
+package Day3LineComparision;
+
+public class LineComputation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome to Line Comparision Program");
+	}
+
+}
